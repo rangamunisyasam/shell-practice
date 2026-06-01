815cc65c64834b7e6769eb7e6742a470e1a9a453
Rangamuni Syasam <rangamunisyasam88@gmail.com>
2026-06-01 07:55:04 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Suresh: : Hello Ramesh, How are you?"
+echo "Ramesh : : Hi Suresh, I am fine thanks for asking. How are you doing?"
+echo "Suresh : : doing fine, what are you learing Ramesh?"
+echo "Ramesh : : I am learing shell. What about you?"
+
