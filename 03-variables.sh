4a56bc15b36deba43edb6a3920912bad0de74b27
Rangamuni Syasam <rangamunisyasam88@gmail.com>
2026-06-02 19:58:08 +0530

    actions

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PERSON1=Suresh  #no space between value and =
-PERSON2=Ramesh
+PERSON1=Trump  #no space between value and =
+PERSON2=Putin
 
-echo "$PERSON1: : Hello $PERSON2, How are you?"
+echo "$PERSON1 : : Hello $PERSON2, How are you?"
 echo "$PERSON2 : : Hi $PERSON1, I am fine thanks for asking. How are you doing?"
 echo "$PERSON1 : : doing fine, what are you learing $PERSON2?"
 echo "$PERSON2 : : I am learing shell. What about you?"
