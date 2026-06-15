136ace490c602d55c226c0f2d7867d0bbec18c40
Rangamuni Syasam <rangamunisyasam88@gmail.com>
2026-06-15 22:38:36 +0530

    actions

---
+++ 12-logs.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+
+USERID=$(id -u)
+LOGS-FOLDER="/var/log/shell-script"
+LOGS-FILE="var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+echo "Please run this script with root user access"
+exit 1
+fi
+
+mkdir -p $LOGS-FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+
+dnf install nginx -y &>> $LOGS-FILE
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y &>> $LOGS-FILE
+VALIDATE $? "Installing mysql"
+
+dnf install nodejs -y &>> $LOGS-FILE
+VALIDATE $? "Installing nodejs"
