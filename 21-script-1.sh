4747c2b7fe182f57894a0ca073c381885e29423a
Rangamuni Syasam <rangamunisyasam88@gmail.com>
2026-09-19 21:23:13 +0530

    action

---
+++ 21-script-1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COUNTRY=INDIA
+
+echo "i am in: $COUNTRY"
+echo "PID of SCRIPT-1: $$"
+
+sh script-22.sh
---
+++ 22-script-2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COUNTRY=UAE
+
+echo "i am in: $COUNTRY"
+echo "PID of SCRIPT-2: $$"
